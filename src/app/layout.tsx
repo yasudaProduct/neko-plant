26a859d6368712456290275a895b324f0783bc9c
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-05 18:51:18 +0900

    :lipstick: 背景色

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-green-50">
           <Header />
           {children}
           <Footer />

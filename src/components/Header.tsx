57b750715299faaa290bb32e2c4f173e6684159b
yasudaProduct <yasu.private.ct@gmail.com>
2025-01-06 01:43:52 +0900

    :lipstick: ヘッダーメニュー追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,18 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Leaf } from "lucide-react";
+import { House, Leaf, LogOut, Wrench } from "lucide-react";
 import useUser from "@/hooks/useUser";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { User } from "@supabase/supabase-js";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function Header() {
-  const { session, signOut } = useUser();
+  const { session } = useUser();
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     setUser(session?.user || null);
-    console.log("session", session);
-    console.log("user", user);
   }, [session]);
 
   return (
@@ -31,14 +30,120 @@ export default function Header() {
             </Link>
           </Button>
         ) : (
-          <Button
-            className="bg-accent text-accent-foreground hover:bg-accent/90"
-            onClick={signOut}
-          >
-            ログアウト
-          </Button>
+          <DropdownMenu
+            userImage={user.user_metadata.avatar_url || ""}
+            aliasId={user.user_metadata.default_alias_id || ""}
+            userName={user.user_metadata.name || ""}
+          />
         )}
       </div>
     </header>
   );
 }
+
+interface DropdownMenuProps {
+  userImage: string;
+  aliasId: string;
+  userName: string;
+}
+
+const DropdownMenu: React.FC<DropdownMenuProps> = ({
+  userImage,
+  aliasId,
+  userName,
+}) => {
+  // const router = useRouter();
+  const { signOut } = useUser();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      setIsOpen(false);
+    };
+  }, []);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setIsOpen(false);
+    }
+  };
+
+  const handleMenuItemClick = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div ref={dropdownRef} className="relative">
+      <button
+        onClick={toggleDropdown}
+        onKeyDown={handleKeyDown}
+        className="focus:outline-none"
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+      >
+        <Avatar className="w-8 h-8">
+          <AvatarImage src={userImage || undefined} alt={userName || "User"} />
+          <AvatarFallback>{userName.charAt(0) || "U"}</AvatarFallback>
+        </Avatar>
+      </button>
+      {isOpen && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-60 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            <Link legacyBehavior href={"/" + aliasId}>
+              <a
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex items-center"
+                role="menuitem"
+                onClick={handleMenuItemClick}
+              >
+                <House className="w-6 h-6 mr-2" />
+                マイページ確認
+              </a>
+            </Link>
+            <Link legacyBehavior href={"/settings"}>
+              <a
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex items-center"
+                role="menuitem"
+                onClick={handleMenuItemClick}
+              >
+                <Wrench className="w-6 h-6 mr-2" />
+                設定
+              </a>
+            </Link>
+            <button
+              onClick={() => {
+                signOut();
+                handleMenuItemClick();
+              }}
+              className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+            >
+              <span className=" flex items-center">
+                <LogOut className="w-6 h-6 mr-2" />
+                ログアウト
+              </span>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};

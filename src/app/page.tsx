cea3773e51170eae707491e24321bb53c098de9b
yasudaProduct <yasu.private.ct@gmail.com>
2025-02-24 14:43:08 +0900

    ✨ Prisma と Supabase を使用してサーバー側プラント取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,18 @@
 
 import PlantCard from "@/components/PlantCard";
 import { Input } from "@/components/ui/input";
-import { plantData } from "@/lib/mock-data";
-import { createClient } from "@/lib/supabase/client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-
-type Plant = {
-  id: number;
-  name: string;
-  image: string;
-};
+import { Plant } from "./types/plant";
+import { getPlants } from "@/actions/plant-action";
 
 export default function Home() {
-  const supabase = createClient();
   const [plants, setPlants] = useState<Plant[]>([]);
 
   useEffect(() => {
-    console.log("fetching plants");
     const fetchPlants = async () => {
-      const { data, error } = await supabase
-        .from("plants")
-        .select("id, name, image");
-
-      if (error) {
-        console.error("Error fetching plants:", error);
-      } else {
-        console.log("data", data);
-        setPlants(data);
-      }
+      const plants = await getPlants();
+      setPlants(plants);
     };
     fetchPlants();
   }, []);
@@ -64,7 +48,7 @@ export default function Home() {
               <Link key={plant.id} href={`/plants/${plant.id}`}>
                 <PlantCard
                   name={plant.name}
-                  imageSrc={plant.image}
+                  imageSrc={plant.imageUrl ?? ""}
                   isSafe={true}
                   likes={0}
                   dislikes={0}

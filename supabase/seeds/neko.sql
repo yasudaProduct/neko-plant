04da65453a846ace10b09024162a63a3eecc2e04
yasudaProduct <yasu.private.ct@gmail.com>
2025-03-16 20:37:26 +0900

    🌱 シード追加

--- supabase/seeds/neko.sql
+++ supabase/seeds/neko.sql
@@ -13,3 +13,36 @@ INSERT INTO neko (name, image) VALUES
 
 INSERT INTO neko (name, image) VALUES 
 ('サイベリアン', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('アビシニアン', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('アメリカンカール', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('アメリカンショートヘアー', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('アメリカンワイヤーヘアー', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('エキゾチック', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('エジプシャンウマ', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('オシキャット', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('オリエンタルショートエアー', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('キムリック', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('コーニッシュ・レックス', NULL);
+
+INSERT INTO neko (name, image) VALUES 
+('コラット', NULL);
--- supabase/seeds/plants.sql
+++ supabase/seeds/plants.sql
@@ -7,3 +7,33 @@ INSERT INTO plants (name, image_src) VALUES
 
 INSERT INTO plants (name, image_src) VALUES 
 ('エバーフレッシュ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('モンステラ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('マドカズラ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('クワズイモ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('アロエ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('アグラオネマ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('ビカクシダ', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('カラテア', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('ペペロミア', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('フィカス', NULL);
+
+INSERT INTO plants (name, image_src) VALUES 
+('ガジュマル', NULL);
